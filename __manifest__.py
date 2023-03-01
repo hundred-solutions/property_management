c2b6e255461ec11364e7b3ae6bb8a9f8d5657f1a
Harsh Chandrakar <harsh.c@hundredsolutions.com>
2023-03-01 16:14:05 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Property Management',
+    'summary': """Amenities management tool for companies""",
+    'description': """It is a tool which will help company to manage company properties""",
+    'website': 'https://www.hundredsolutions.com/',
+    'category':"Employees",
+    'depends': [
+        'base',
+        'hr',
+        'product'
+    ],
+    'data': [
+        "views/property_management.xml",
+    ],
+    'demo': [
+    ],
+    'images': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ model/property.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from odoo import models,fields
+
+class PropertyManagement(models.Model):
+     _inherit = "hr.employee"
+
+     given_materials_line_ids = fields.One2many("given.materials.line","given_materials_line_id",string="Property")
+
+
+class Property(models.Model):
+     _name = "given.materials.line"
+
+     given_materials_line_id = fields.Many2one("hr.employee",string="Property")
+     name = fields.Many2one("product.template",string="Name")
+     assign_date = fields.Datetime("Issue date",readonly=True,default=datetime.today())
+     qty= fields.Integer("Quantity")
+     given_condition = fields.Selection([("new","New"),("used","Used")],string="Given Condition")
+     returned = fields.Boolean(string="Returned")
+     return_date = fields.Datetime(string="Return Date")
+     return_condition = fields.Selection([("normal","Normal"),("damaged","Damaged")],string="Return Condition")
+
+class Product(models.Model):
+
+     _inherit = "product.template"
+
+     employee_use = fields.Boolean(string="Employee Use")
